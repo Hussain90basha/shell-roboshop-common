7639080d13fc7118fea29afc18e107df56cce8a9
Hussain90basha <bashahussain9010@gmail.com>
2025-10-23 16:55:41 +0530

    roboshop-common

--- common.sh
+++ common.sh
@@ -11,6 +11,7 @@ SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 START_TIME=$(date +%s)
 MONGODB_HOST=mongodb.eliyas.fun
+SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
